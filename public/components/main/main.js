53d419bc926113b80ee72d4fc538d16e604ef4d6
Elena <elenam19396@gmail.com>
2020-03-05 19:54:19 +0100

    Checkboxes included

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import elasticsearch from 'elasticsearch'
 import {
   EuiPage,
   EuiPageHeader,
@@ -8,24 +9,47 @@ import {
   EuiPageContentHeader,
   EuiPageContentBody,
   EuiText,
+  EuiCheckboxGroup,
 } from '@elastic/eui';
+
+const checkboxes = [
+  {
+    id: 'checkBox1',
+    label: 'Check Box 1',
+  },
+  {
+    id: 'checkBox2',
+    label: 'Check Box 2',
+  },
+  {
+    id: 'checkBox3',
+    label: 'Check Box 3',
+  },
+  {
+    id: 'checkBox4',
+    label: 'Check Box 4',
+  },
+];
+
 
 export class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      checkboxIdToSelectedMap: {
+      },
+    };
   }
+  onChange(optionId) {
+    const newCheckboxIdToSelectedMap = {... this.state.checkboxIdToSelectedMap}
 
-  componentDidMount() {
-    /*
-       FOR EXAMPLE PURPOSES ONLY.  There are much better ways to
-       manage state and update your UI than this.
-    */
-    const { httpClient } = this.props;
-    httpClient.get('../api/starcoordinates/example').then(resp => {
-      this.setState({ time: resp.data.time });
+    newCheckboxIdToSelectedMap[optionId]= !newCheckboxIdToSelectedMap[optionId];
+
+    this.setState({
+      checkboxIdToSelectedMap: newCheckboxIdToSelectedMap,
     });
-  }
+  };
+
   render() {
     const { title } = this.props;
     return (
@@ -47,6 +71,11 @@ export class Main extends React.Component {
                 <h3>You have successfully created your first Kibana Plugin!</h3>
                 <p>The server time (via API call) is {this.state.time || 'NO API CALL YET'}</p>
               </EuiText>
+              <EuiCheckboxGroup
+                options={checkboxes}
+                idToSelectedMap={this.state.checkboxIdToSelectedMap}
+                onChange={(optionId) => (this.onChange(optionId))}
+              />
             </EuiPageContentBody>
           </EuiPageContent>
         </EuiPageBody>
