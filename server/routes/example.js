4821e009f38f73767a37afd604a98bd8b804cd39
Elena <elenam19396@gmail.com>
2020-12-07 18:36:34 +0100

    removed example call from server

--- server/routes/example.js
+++ server/routes/example.js
@@ -3,30 +3,6 @@ export default function (server){
   const {callWithRequest} = server.plugins.elasticsearch.getCluster('data');
 
   server.route({
-    path:'/api/starcoordinates/example',
-    method: 'GET',
-    handler: async function (req, res){
-      let resp = {}
-      try{
-        resp = await callWithRequest(req, 'search', {
-          index: 'hot100billboardaudiofeatures',
-          size: 10,
-          body: {
-            query: {
-              match: {
-                spotify_genre: "pop"
-              }
-            }
-          }
-        })
-      } catch (errResp){
-        resp = errResp
-      }
-      return {body: resp}
-    }
-  });
-
-  server.route({
     path:'/api/starcoordinates/example/getIndices',
     method: 'GET',
     handler: async function (req, res){
